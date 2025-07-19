21433187ed4aad3e21ff2831ac94ef4eff4b895f
20♥ <nuttapong@npr.digital>
2025-07-20 03:06:21 +0700

    add: api document

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,17 @@ async function bootstrap() {
     origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+  .setTitle('Movie API')
+  .setDescription('Nextflix API Documentation')
+  .setVersion('1.0')
+  .addTag('movies')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
